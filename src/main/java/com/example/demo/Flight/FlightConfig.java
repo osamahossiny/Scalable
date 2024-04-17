ed94a9efafc95e43b944cc27a2635a9b4533cc92
BavarianAT <ataha3412@gmail.com>
2024-04-18 01:18:50 +0200

    Temp(Airline-Flight) Config files

--- src/main/java/com/example/demo/Flight/FlightConfig.java
+++ src/main/java/com/example/demo/Flight/FlightConfig.java
@@ -10,9 +10,9 @@ public class FlightConfig {
     @Bean
     CommandLineRunner commandLineRunner(FlightRepository repository){
         return args -> {
-//            Flight cairo_berlin = new Flight("Egypt","Germany", LocalTime.now(), LocalTime.now(), 20L,500.4F,10000.0F, 1000.0F, 500.0F, "CAI", "BER", LocalDate.now(), LocalDate.now());
-//            Flight berlin_cairo = new Flight(100L,"Germany","Egypt", LocalTime.now(), LocalTime.now(), 20L,500.4F,10000.0F, 1000.0F, 500.0F, "BER", "CAI", LocalDate.now(), LocalDate.now());
-//            repository.saveAll(List.of(cairo_berlin, berlin_cairo));
+            Flight cairo_berlin = new Flight("Egypt","Germany", LocalTime.now().toString(), LocalTime.now().toString(), 20L,500.4F,10000.0F, 1000.0F, 500.0F, "CAI", "BER", LocalDate.now().toString(), LocalDate.now().toString());
+            Flight berlin_cairo = new Flight(100L,"Germany","Egypt", LocalTime.now().toString(), LocalTime.now().toString(), 20L,500.4F,10000.0F, 1000.0F, 500.0F, "BER", "CAI", LocalDate.now().toString(), LocalDate.now().toString());
+            repository.saveAll(List.of(cairo_berlin, berlin_cairo));
         };
     }
 }
