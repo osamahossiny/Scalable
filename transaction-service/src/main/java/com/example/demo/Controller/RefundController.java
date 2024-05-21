531f01bfdd47b1469b97afa4fcdbc33f00d33bf5
ahmed elblatgy <ahmedelblatgy125@gmail.com>
2024-05-21 21:33:37 +0300

    add refundrequest in trnascation service

---
+++ transaction-service/src/main/java/com/example/demo/Controller/RefundController.java
@@ -0,0 +1,31 @@
+package com.example.demo.Controller;
+
+import com.example.demo.model.Refund;
+import com.example.demo.Service.RefundService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/refunds")
+public class RefundController {
+
+    @Autowired
+    private RefundService refundService;
+
+    @PostMapping
+    public ResponseEntity<Refund> createRefund(@RequestBody Refund refund) {
+        //String jwt = token.substring(7);
+       // String username = jwtUtil.extractUsername(jwt);
+        // Assuming you have a method to get userId from username
+        // add auth to get user_id
+         Long userId = 1L;
+        refund.setUserId(userId);
+        Refund createdRefund = refundService.createRefund(refund);
+        return ResponseEntity.ok(createdRefund);
+    }
+
+
+}
